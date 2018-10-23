80a5c402d868ef6761988d057ebb5a4edb34a81d
Pawel Ochota <pawel.ochota@gmail.com>
2018-10-23 20:55:19 +0200

    Remove sky enitity

--- src/components/scene/scene.js
+++ src/components/scene/scene.js
@@ -16,10 +16,9 @@ const SceneWrapper = styled.div`
 
 const Scene = () => (
   <SceneWrapper>
-    <a-scene embedded stats vr-mode-ui="enabled: false">
-      <a-box position="-1 4 -3" rotation="0 45 0" color="#4CC3D9" dynamic-body />
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" static-body />
-      <a-sky color="#ECECEC" />
+    <a-scene embedded stats vr-mode-ui="enabled: false" background="color: #eee">
+      <a-box position="-1 4 -3" rotation="0 45 0" color="#4CC3D9" />
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" />
     </a-scene>
   </SceneWrapper>
 );

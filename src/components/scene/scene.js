ef728ea5d0fc617f5e1beadca9333dea12163ed5
Michał Winiarski <m.winiarski.z@gmail.com>
2018-10-26 20:24:43 +0200

    Added additional boxes

--- src/components/scene/scene.js
+++ src/components/scene/scene.js
@@ -17,7 +17,9 @@ const SceneWrapper = styled.div`
 const Scene = () => (
   <SceneWrapper>
     <a-scene embedded stats vr-mode-ui="enabled: false" background="color: #eee">
-      <a-box position="-1 4 -3" rotation="0 45 0" color="#4CC3D9" />
+      <a-box position="-1 2 -4" rotation="0 0 0" color="red" />
+      <a-box position="1 2 -4" rotation="0 0 0" color="green" />
+      <a-box position="0 2 -4" rotation="0 0 0" color="blue" />
       <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" />
     </a-scene>
   </SceneWrapper>
